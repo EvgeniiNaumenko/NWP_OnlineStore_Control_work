cf480e2863f283c8fd31eebf3073103c3e73a7e3
Denis <bigdramashow730@gmail.com>
2024-10-30 00:02:02 +0200

    commit commit

--- OnlineStoreServer/Models/ApplicationDbContext.cs
+++ OnlineStoreServer/Models/ApplicationDbContext.cs
@@ -8,16 +8,16 @@ namespace OnlineStoreServer.Models
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductDescription> ProductDescriptions { get; set; }
         public DbSet<ProductImage> ProductImages { get; set; }
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    //optionsBuilder.UseSqlServer(@"Server=DESKTOP-C317JNM;Database=OnlineStoreDB;Trusted_Connection=True;TrustServerCertificate=True;");   // EvgeniiPath
-        //    optionsBuilder.UseSqlServer(@"Server=DESKTOP-V6G1V7P;Database=OnlineStoreDB;Trusted_Connection=True;TrustServerCertificate=True;");     // DenisPath
-        //}
-
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            //optionsBuilder.UseSqlServer(@"Server=DESKTOP-C317JNM;Database=OnlineStoreDB;Trusted_Connection=True;TrustServerCertificate=True;");   // EvgeniiPath
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-V6G1V7P;Database=OnlineStoreDB;Trusted_Connection=True;TrustServerCertificate=True;");     // DenisPath
         }
+
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        //    : base(options)
+        //{
+        //}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {

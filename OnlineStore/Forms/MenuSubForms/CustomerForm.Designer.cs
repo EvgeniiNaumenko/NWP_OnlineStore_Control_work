561eab27127e182b1d71e5fc93c0f56e6214a7c8
Denis <bigdramashow730@gmail.com>
2024-10-30 17:41:23 +0200

    add form color

--- OnlineStore/Forms/MenuSubForms/CustomerForm.Designer.cs
+++ OnlineStore/Forms/MenuSubForms/CustomerForm.Designer.cs
@@ -44,6 +44,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(34, 33, 74);
             ClientSize = new Size(800, 450);
             Controls.Add(button1);
             Name = "CustomerForm";
--- OnlineStore/Forms/MenuSubForms/SettingsForm.Designer.cs
+++ OnlineStore/Forms/MenuSubForms/SettingsForm.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SettingsForm";
+            SuspendLayout();
+            // 
+            // SettingsForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(34, 33, 74);
+            ClientSize = new Size(800, 450);
+            Name = "SettingsForm";
+            Text = "SettingsForm";
+            ResumeLayout(false);
         }
 
         #endregion

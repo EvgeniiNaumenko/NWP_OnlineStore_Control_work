10633a09d6664d3f635d102a45c66ed68917af8e
Naumenko Evgenii <evgeniynaumenko2017@gmail.com>
2024-10-30 18:03:21 +0200

    fix and changes

--- OnlineStore/Program.cs
+++ OnlineStore/Program.cs
@@ -13,8 +13,7 @@ namespace OnlineStore
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //Application.Run(new Login());
-            Application.Run(new UserInterfaceForm());
+            Application.Run(new Login());
         }
     }
 }

d7c4c38e3c63b93c084ff0cfcebbdb786d2283f8
Naumenko Evgenii <evgeniynaumenko2017@gmail.com>
2024-10-31 21:22:06 +0200

    add Dachboard and search

--- OnlineStore/Forms/MenuSubForms/DashboardForm.cs
+++ OnlineStore/Forms/MenuSubForms/DashboardForm.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using FontAwesome.Sharp;
+using OnlineStore.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +16,282 @@ namespace OnlineStore.Forms.MenuSubForms
 {
     public partial class DashboardForm : Form
     {
+        public static List<Product> filterProducts;
+        private static readonly HttpClient client = new HttpClient();
+        private static int currentPage = 1; 
+        private const int PageSize = 12;
+        private TextBox nameTextBox;
+        private TextBox categoryTextBox;
+        private TextBox minPriceTextBox;
+        private TextBox maxPriceTextBox;
+
         public DashboardForm()
         {
             InitializeComponent();
+            InitializeSearchPanel();
+        }
+
+
+        //var filterRequest = new ProductFilterRequest
+        //{
+        //    Name = nameInput.Text,
+        //    Category = categoryInput.Text,
+        //    MinPrice = string.IsNullOrEmpty(minPriceInput.Text) ? (decimal?)null : decimal.Parse(minPriceInput.Text),
+        //    MaxPrice = string.IsNullOrEmpty(maxPriceInput.Text) ? (decimal?)null : decimal.Parse(maxPriceInput.Text),
+        //    Page = currentPage // Текущая страница
+        //};
+
+        //var response = await httpClient.PostAsJsonAsync("https://localhost:7284/products/filter", filterRequest);
+        //var products = await response.Content.ReadFromJsonAsync<List<Product>>();
+
+        private void InitializeSearchPanel()
+        {
+            TableLayoutPanel tableLayoutPanel = new TableLayoutPanel
+            {
+                Dock = DockStyle.Fill,
+                ColumnCount = 6, // Пять полей + кнопка поиска
+                RowCount = 1,
+                Padding = new Padding(0)
+            };
+
+            for (int i = 0; i < 4; i++)
+            {
+                tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            }
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 60F));
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 60F));
+
+            nameTextBox = new TextBox
+            {
+                PlaceholderText = "Name",
+                Dock = DockStyle.Fill,
+                BackColor = this.BackColor,
+                Height = 40,
+                Margin = new Padding(0, 10, 0, 0),
+                ForeColor = Color.Gainsboro,
+                MinimumSize = new Size(100, 30)
+
+            };
+            categoryTextBox = new TextBox
+            {
+                PlaceholderText = "Category",
+                Dock = DockStyle.Fill,
+                BackColor = this.BackColor,
+                Height = 40,
+                Margin = new Padding(0, 10, 0, 0),
+                ForeColor = Color.Gainsboro,
+                MinimumSize = new Size(100, 30)
+            };
+            minPriceTextBox = new TextBox
+            {
+                PlaceholderText = "Min Price",
+                Dock = DockStyle.Fill,
+                BackColor = this.BackColor,
+                Height = 40,
+                Margin = new Padding(0, 10, 0, 0),
+                ForeColor = Color.Gainsboro,
+                MinimumSize = new Size(100, 30)
+            };
+            maxPriceTextBox = new TextBox
+            {
+                PlaceholderText = "Max Price",
+                Dock = DockStyle.Fill,
+                BackColor = this.BackColor,
+                Height = 40,
+                Margin = new Padding(0, 10, 0, 0),
+                ForeColor = Color.Gainsboro,
+                MinimumSize = new Size(100, 30)
+            };
+
+            tableLayoutPanel.Controls.Add(nameTextBox, 0, 0);
+            tableLayoutPanel.Controls.Add(categoryTextBox, 1, 0);
+            tableLayoutPanel.Controls.Add(minPriceTextBox, 2, 0);
+            tableLayoutPanel.Controls.Add(maxPriceTextBox, 3, 0);
+
+
+            FilterPanel.Controls.Add(tableLayoutPanel);
+            this.Controls.Add(FilterPanel);
+        }
+
+        private async Task<List<Product>> FetchFilteredProducts(ProductFilterRequest filterRequest)
+        {
+            try
+            {
+                var response = await client.PostAsJsonAsync("https://localhost:7284/products/filter", filterRequest);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadFromJsonAsync<List<Product>>();
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка при получении продуктов: " + response.ReasonPhrase);
+                    return new List<Product>(); 
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+                return new List<Product>();
+            }
+        }
+
+        private async void NextPage()
+        {
+            currentPage++;
+            await LoadProducts();
+        }
+
+        private async void PreviousPage()
+        {
+            if (currentPage > 1)
+            {
+                currentPage--;
+                await LoadProducts(); 
+            }
+        }
+        private async Task LoadProducts()
+        {
+
+            var filterRequest = new ProductFilterRequest
+            {
+                Name = nameTextBox.Text,
+                Category = categoryTextBox.Text,
+                MinPrice = string.IsNullOrEmpty(minPriceTextBox.Text) ? (decimal?)null : decimal.Parse(minPriceTextBox.Text),
+                MaxPrice = string.IsNullOrEmpty(maxPriceTextBox.Text) ? (decimal?)null : decimal.Parse(maxPriceTextBox.Text),
+                Page = currentPage,
+                PageSize = PageSize
+            };
+
+            var products = await FetchFilteredProducts(filterRequest);
+            filterProducts = products;
+            CreateCards(filterProducts);
+        }
+        private async void FilterBtn_Click(object sender, EventArgs e)
+        {
+            currentPage = 1;
+            LoadProducts();
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            nameTextBox.Text = "";
+            categoryTextBox.Text = "";
+            minPriceTextBox.Text = "";
+            maxPriceTextBox.Text = "";
+        }
+
+        private void BackwardBtn_Click(object sender, EventArgs e)
+        {
+            NextPage();
+        }
+
+        private void ForwardBtn_Click(object sender, EventArgs e)
+        {
+            PreviousPage();
+        }
+
+        private async void CreateCards(List<Product> list)
+        {
+            FilterProductsPanel.Controls.Clear();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                MessageBox.Show($"Создание карточки для продукта: {list[i].Name}");
+                Panel cardPanel = new Panel
+                {
+                    Size = new Size(300, 400),
+                    BorderStyle = BorderStyle.FixedSingle,
+                    BackColor = Color.MintCream,
+                    Margin = new Padding(25),
+                    Padding = new Padding(5),
+                    Tag = i
+                };
+
+                PictureBox pictureBox = new PictureBox
+                {
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    Height = cardPanel.Height / 2,
+                    Dock = DockStyle.Top,
+                };
+
+                string imageUrl = list[i].imageUrl;
+                try
+                {
+                     var imageStream = await client.GetStreamAsync(imageUrl);
+                     pictureBox.Image = Image.FromStream(imageStream);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка загрузки изображения: {ex.Message}");
+                }
+                TextBox descriptionTextBox = new TextBox
+                {
+                    Multiline = true,
+                    ScrollBars = ScrollBars.Vertical,
+                    Text = $"{list[i].Name} \n {list[i].Category} \n {list[i].Description}",
+                    ReadOnly = true,
+                    BackColor = Color.MintCream,
+                    BorderStyle = BorderStyle.None,
+                    Dock = DockStyle.Fill,
+                };
+
+                Label priceLabel = new Label
+                {
+                    Text = $"Price: {list[i].Price.ToString()}",
+                    Dock = DockStyle.Bottom,
+                    TextAlign = ContentAlignment.BottomLeft,
+                    Font = new Font("Arial", 14, FontStyle.Italic),
+                    AutoSize = true
+
+                };
+
+                Label nameLabel = new Label
+                {
+                    Text = list[i].Name.ToString(),
+                    Dock = DockStyle.Top,
+                    TextAlign = ContentAlignment.BottomLeft,
+                    Font = new Font("Arial", 16, FontStyle.Bold | FontStyle.Underline),
+                    AutoSize = true
+
+                };
+
+                Label categoryLabel = new Label
+                {
+                    Text = list[i].Name.ToString(),
+                    Dock = DockStyle.Top,
+                    TextAlign = ContentAlignment.BottomLeft,
+                    Font = new Font("Arial", 14, FontStyle.Italic),
+                    AutoSize = true
+
+                };
+
+                Button addBtn = new Button
+                {
+                    Text = "В корзину",
+                    Dock = DockStyle.Bottom,
+                    Size = new Size(70, 30),
+                    Margin = new Padding(5),
+                    BackColor = Color.FromArgb(41, 128, 185),
+                    FlatStyle = FlatStyle.Flat,
+                    Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0),
+                    ForeColor = Color.White,
+                    Tag = i
+                };
+                addBtn.Click += async (s, e) =>
+                {
+                    
+                };
+
+                cardPanel.Controls.Add(descriptionTextBox);
+                cardPanel.Controls.Add(categoryLabel);
+                cardPanel.Controls.Add(nameLabel);
+                cardPanel.Controls.Add(pictureBox);
+                cardPanel.Controls.Add(priceLabel);
+                cardPanel.Controls.Add(addBtn);
+
+                FilterProductsPanel.Controls.Add(cardPanel);
+            }
         }
     }
 }
---
+++ OnlineStore/Models/ProductFilterRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Models
+{
+    public class ProductFilterRequest
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 12; 
+    }
+}
